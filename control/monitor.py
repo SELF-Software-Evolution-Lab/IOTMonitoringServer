43da08f546d6356b94ef49a256e5437a3dd17c39
Juan Avelino <juan.avelino@imagineapps.co>
2022-06-02 14:56:42 -0500

    Intervalo de revisión de datos actualizado a 5 min

--- control/monitor.py
+++ control/monitor.py
@@ -102,10 +102,10 @@ def setup_mqtt():
 
 def start_cron():
     '''
-    Inicia el cron que se encarga de ejecutar la función analyze_data cada minuto.
+    Inicia el cron que se encarga de ejecutar la función analyze_data cada 5 minutos.
     '''
     print("Iniciando cron...")
-    schedule.every().hour.do(analyze_data)
+    schedule.every(5).minutes.do(analyze_data)
     print("Servicio de control iniciado")
     while 1:
         schedule.run_pending()
