79bc0f910580dee93a7d15aa784a7f9ef6d61d4b
alexyirsa <v.yirsa@uniandes.edu.co>
2023-11-04 22:29:53 -0500

    Prueba3

--- receiver/models.py
+++ receiver/models.py
@@ -22,6 +22,13 @@ class City2(models.Model):
     def str(self):
         return "{}".format(self.name)
 
+class City2(models.Model):
+    name = models.CharField(max_length=50, unique=True, blank=False)
+    code = models.CharField(max_length=50, null=True)
+
+    def str(self):
+        return "{}".format(self.name)
+
 class State(models.Model):
     name = models.CharField(max_length=50, unique=False, blank=False)
     code = models.CharField(max_length=50, null=True)
